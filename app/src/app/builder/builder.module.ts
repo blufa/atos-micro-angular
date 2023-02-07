8bd52063b0024fdf4726a6b43959616b8bac6f06
Momar Cisse <momar@thepinkchannel.com>
2023-02-07 23:18:49 +0000

    Added builder module components

--- app/src/app/builder/builder.module.ts
+++ app/src/app/builder/builder.module.ts
@@ -2,10 +2,29 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BuilderRoutingModule } from './builder-routing.module';
+import { PersonalInformationFormComponent } from './personal-information-form/personal-information-form.component';
+import { EducationFormComponent } from './education-form/education-form.component';
+import { LeisuresFormComponent } from './leisures-form/leisures-form.component';
+import { CertificationsFormComponent } from './certifications-form/certifications-form.component';
+import { SkillsFormComponent } from './skills-form/skills-form.component';
+import { ReferencesFormComponent } from './references-form/references-form.component';
+import { LanguagesFormComponent } from './languages-form/languages-form.component';
+import { ExperiencesFormComponent } from './experiences-form/experiences-form.component';
+import { AwardsHonorsFormComponent } from './awards-honors-form/awards-honors-form.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PersonalInformationFormComponent,
+    EducationFormComponent,
+    LeisuresFormComponent,
+    CertificationsFormComponent,
+    SkillsFormComponent,
+    ReferencesFormComponent,
+    LanguagesFormComponent,
+    ExperiencesFormComponent,
+    AwardsHonorsFormComponent
+  ],
   imports: [
     CommonModule,
     BuilderRoutingModule
---
+++ app/src/app/builder/references-form/references-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-references-form',
+  templateUrl: './references-form.component.html',
+  styleUrls: ['./references-form.component.scss']
+})
+export class ReferencesFormComponent {
+
+}
