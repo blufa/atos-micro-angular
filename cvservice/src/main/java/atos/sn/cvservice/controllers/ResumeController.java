27586cf2cf4a6c26ae782701325cbc3eb324a1bc
amadou98 <amadoubhoyediallo98@gmail.com>
2023-02-11 22:41:25 +0000

    microservices configuration

--- cvservice/src/main/java/atos/sn/cvservice/controllers/ResumeController.java
+++ cvservice/src/main/java/atos/sn/cvservice/controllers/ResumeController.java
@@ -3,6 +3,7 @@ package atos.sn.cvservice.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
